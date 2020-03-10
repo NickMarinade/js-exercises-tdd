75f88b8f256d3131df13e752fa993492e541305a
Mykola Marenich <marenich.mykola@gmail.com>
2020-03-10 22:59:31 +0100

    06

--- II.write-tests/06-largest-number/largest-number.test.js
+++ II.write-tests/06-largest-number/largest-number.test.js
@@ -1,3 +1,17 @@
+let largest = require("./largest-number");
+
+test("Get largest number", function () {
+
+    // Arrange
+    let input = [3, 21, 88, 4, 36];
+    let unchanged = [3, 21, 88, 4, 36];
+    let expected = 88;
+    // Act
+    let output = largest(input);
+    // Assert
+    expect(output).toEqual(expected);
+    expect(input).toEqual(unchanged);
+});
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
